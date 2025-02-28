ead4b5135dbe5e886c3c76de00317be6e62287e9
elizawura <elizabethsampong@yahoo.com>
2025-02-28 13:03:23 +0000

    third commit

--- js/script.js
+++ js/script.js
@@ -96,3 +96,8 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+// var blink = document.getElementById("blink");
+// setInterval(function () {
+//   blink.style.opacity = blink.style.opacity == 0 ? 1 : 0;
+// }, 1500);
